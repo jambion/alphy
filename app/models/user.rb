f79da6eb3b2853b6fa86f5804ca4e5e9b0eeae9d
jambion <ambion_j@yahoo.com>
2017-08-04 12:51:45 -0700

    Signup User From UI

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base 
   has_many :articles
-  
+  has_secure_password
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
@@ -9,5 +9,5 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false }, 
             format: {with: VALID_EMAIL_REGEX }
             
-  has_secure_password
+  
 end
